c723d8d72ecb0e4062c7bb18f7be34a7d16bced8
Denis Kalyuzhnyy <denis.kaluzhnyy@gmail.com>
2020-11-28 17:19:08 +0300

    [+] update

--- generate_versions.sh
+++ generate_versions.sh
@@ -1,4 +1,42 @@
 #!/usr/bin/env bash
+# This script
 
-(find . -name "PKGBUILD" -print0 | xargs -0 grep 'pkgver=' | \
-    sed -E 's%^\./([^/]+)/.*pkgver="([^"]+)"%\1 \2%' | sort) 2>/dev/null >versions
+
+function generate_current_versions(){
+    find . -name "PKGBUILD" -print0 | xargs -0 grep 'pkgver=' | \
+        sed -E 's%^\./([^/]+)/.*pkgver="([^"]+)"%\1 \2%' | sort
+}
+
+# Public funcitons
+function update(){
+    mkdir -p tmp
+    generate_current_versions 2>/dev/null >tmp/versions
+    while read -r line
+    do
+        is_updated=$(echo "$line" | jq .event | tr -d \")
+        pkg=$(echo "$line" | jq .name | tr -d \")
+        pkgver=$(echo "$line" | jq .version | tr -d \")
+
+        if [ "$is_updated" = "updated" ]; then
+            pkgold=$(echo "$line" | jq .old_version | tr -d \")
+            # if [ -f "./${pkg}/download.sh" ]; then
+            #     (
+            #         echo "Download $pkg"
+            #         cd "$pkg"
+            #         export PGKVER="$pkgver"
+            #         ./download.sh
+            #     )
+            # fi
+            sed -i."__${pkgold}" "s/pkgver=.*/pkgver=\"${pkgver}\"/" "${pkg}/PKGBUILD"
+            echo "$pkg updated to $pkgver"
+        fi
+    done < <(nvchecker --logger=json -c source_version.toml)
+}
+
+
+function build(){
+    pacur project build
+    pacur project repo
+}
+
+"$@"
